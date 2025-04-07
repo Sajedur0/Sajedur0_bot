42b0adcb5765068cdb833510f5047f9d1ef0d043
Sajedur Rahman Roni <31769074+Sajedur0@users.noreply.github.com>
2025-04-07 15:15:47 +0600

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,21 @@
 <?php
-// Render-এ এনভায়রনমেন্ট ভেরিয়েবল থেকে টোকেন নিন
-$apiToken = getenv('BOT_TOKEN') ?: "YOUR_BOT_TOKEN";
+$token = "YOUR_BOT_TOKEN"; // টোকেন এখানে বসাও
+$website = "https://api.telegram.org/bot" . $token;
 
-// বাকি কোড অপরিবর্তিত রাখুন...
+$update = file_get_contents("php://input");
+$update = json_decode($update, TRUE);
+
+$chat_id = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+
+if ($message == "/start") {
+    sendMessage($chat_id, "👋 হ্যালো! Render থেকে চালানো PHP বট!");
+} else {
+    sendMessage($chat_id, "তুমি লিখেছো: " . $message);
+}
+
+function sendMessage($chat_id, $message) {
+    global $website;
+    file_get_contents($website."/sendMessage?chat_id=".$chat_id."&text=".urlencode($message));
+}
 ?>
