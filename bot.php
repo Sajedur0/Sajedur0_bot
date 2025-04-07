7ab00381fc4b1d3baac84a2bbf468a1bb3b1d4b8
Sajedur Rahman Roni <31769074+Sajedur0@users.noreply.github.com>
2025-04-07 12:49:03 +0600

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,6 @@
+<?php
+// Render-এ এনভায়রনমেন্ট ভেরিয়েবল থেকে টোকেন নিন
+$apiToken = getenv('BOT_TOKEN') ?: "YOUR_BOT_TOKEN";
+
+// বাকি কোড অপরিবর্তিত রাখুন...
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// Render requires index.php as entry point
+require __DIR__ . '/bot.php';
+?>
